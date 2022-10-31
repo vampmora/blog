71fe7d704ae97907f349d9654f97a1f0ff35b49a
Amr-Elmansoury <amrelmansoury1@gmail.com>
2022-10-31 00:50:20 -0500

    add-file

---
+++ app.js
@@ -0,0 +1,60 @@
+let cnt=0;
+function CountFun(){
+ cnt=parseInt(cnt)+parseInt(1);
+ let divData=document.getElementById("showCount");
+ divData.innerHTML=" "+ cnt +" ";   
+document.divData.classList('.cnt')
+}
+
+let itemTwo = 0;
+function CountFunTwo(){
+    itemTwo = parseInt(itemTwo)+parseInt(1);
+    let item = document.getElementById('showCountTwo');
+    item.innerHTML = " "+ itemTwo + " ";
+    document.item.classList('.itemTwo');
+}
+
+let itemThree = 0;
+function CountFunThree(){
+    itemThree = parseInt(itemThree)+parseInt(1);
+    let item = document.getElementById('showCountThree');
+    item.innerHTML = " "+ itemThree + " ";
+    document.item.classList('.itemThree');
+}
+
+let itemFour = 0;
+function CountFunFour(){
+    itemFour = parseInt(itemFour)+parseInt(1);
+    let item = document.getElementById('showCountFour');
+    item.innerHTML = " "+ itemFour + " ";
+    document.item.classList('.itemFour');
+}
+
+let itemFive = 0;
+function CountFunFive(){
+    itemFive = parseInt(itemFive)+parseInt(1);
+    let item = document.getElementById('showCountFive');
+    item.innerHTML = " "+ itemFive + " ";
+    document.item.classList('.itemFive');
+}
+
+let itemSix = 0;
+function CountFunSix(){
+    itemSix = parseInt(itemSix)+parseInt(1);
+    let item = document.getElementById('showCountSix');
+    item.innerHTML = " "+ itemSix + " ";
+    document.item.classList('.itemSix');
+}
+
+
+
+
+let date = new Date().getFullYear();
+document.getElementById('year').innerHTML = date;
+
+
+
+ 
+
+
+showCountThree
